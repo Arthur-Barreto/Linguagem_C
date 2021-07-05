0a64f27d96c0648c5f6f955176afdb8342247635
Arthur-Barreto <arthurbarreto2905@gmail.com>
2021-07-05 17:15:37 -0300

    configuração do ambiente e teste de memoria de variaveis

---
+++ Basic/tipos_variaveis.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*  função principal  */
+
+main(){
+
+    printf("O tamanho do tipo char eh: %d byte(s)\n",sizeof(char));
+    printf("O tamanho do tipo inteiro eh: %d byte(s)\n",sizeof(int));
+    printf("O tamanho do tipo short int eh: %d byte(s)\n",sizeof(short));
+    printf("O tamanho do tipo long int eh: %d byte(s)\n",sizeof(long));
+    printf("O tamanho do tipo float eh: %d byte(s)\n",sizeof(float));
+    printf("O tamanho do tipo double float eh: %d byte(s)\n",sizeof(double));
+
+} /* end main*/
